f6600c491646c6aff371a7e66607340c6789410a
K Arcadius W <arkadius4991@gmail.com>
2021-01-01 11:45:23 -0600

    changes

---
+++ wikibellum.Data/Migrations/20201231100034_addedHTMLFieldToReccomendations.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace wikibellum.Data.Migrations
+{
+    public partial class addedHTMLFieldToReccomendations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "HTML",
+                table: "BookRecommendation",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HTML",
+                table: "BookRecommendation");
+        }
+    }
+}
